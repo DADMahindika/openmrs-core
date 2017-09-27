842b618c52c3c9961a521097483913bc99cce3d7
Dilki Dandeniya Arachchi <dadmahindika@gmail.com>
2017-09-27 16:21:26 +0530

    Display a warning message
    Display a warning message  to inform that any leading or trailing spaces will be removed when the given name is add to the database.

--- api/src/main/java/org/openmrs/PersonName.java
+++ api/src/main/java/org/openmrs/PersonName.java
@@ -33,8 +33,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JOptionPane;
+import javax.swing.JDialog;
 
 /**
  * A Person can have zero to n PersonName(s).
